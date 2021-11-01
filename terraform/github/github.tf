c88f773d622e6e22194a97eeb39da99a9d6c35c8
Kazuto Kusama <jacopen@gmail.com>
2021-11-02 01:51:32 +0900

    Modify TF

--- terraform/github/github.tf
+++ terraform/github/github.tf
@@ -14,3 +14,16 @@ resource "github_repository" "observability2021" {
   archive_on_destroy = true
   vulnerability_alerts = false
 }
+
+resource "github_repository" "cndt2021" {
+  name        = "cndt2021"
+  description = "CNDT2021に関わるあれこれを管理していくリポジトリ"
+
+  visibility = "private"
+  has_issues = true
+  has_projects = true
+  has_wiki = true
+  auto_init = false
+  archive_on_destroy = true
+  vulnerability_alerts = false
+}
