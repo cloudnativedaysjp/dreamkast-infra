773aabb4dbd4f21c7e29f3c781a57434e96f73c2
mita <iaoiui0727@gmail.com>
2022-01-04 13:27:29 +0900

    Add Auth0 user module

---
+++ terraform/auth0/modules/auth0_users/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    auth0 = {
+      source = "alexkappa/auth0"
+      version = "0.25.0"
+    }
+  }
+}
+
+resource "auth0_user" "this" {
+  connection_name = "Username-Password-Authentication"
+  email = var.email
+  roles = compact([for role_name, users in var.members_of_roles : contains(users, var.email) ? var.role_name_to_role_id[role_name] : null])
+  lifecycle {
+    ignore_changes = [
+      name,
+      connection_name,
+      user_id,
+      email_verified,
+      family_name,
+      given_name
+    ]
+  }
+}
---
+++ terraform/auth0/modules/auth0_users/variables.tf
@@ -0,0 +1,11 @@
+variable email {
+  description = "email address of user"
+}
+
+variable role_name_to_role_id {
+  description = "role_name_to_role_id"
+}
+
+variable "members_of_roles" {
+  description = "members_of_roles"
+}
