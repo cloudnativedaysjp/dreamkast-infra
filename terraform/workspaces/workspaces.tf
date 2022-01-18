13c50f8539f869faad68e86a445c89a922f6a565
Kazuto Kusama <jacopen@gmail.com>
2022-01-19 00:32:55 +0900

    Assign team access

--- terraform/workspaces/workspaces.tf
+++ terraform/workspaces/workspaces.tf
@@ -2,6 +2,11 @@ data "tfe_organization" "org" {
   name = "kusama"
 }
 
+data "tfe_team" "cloudnativedays" {
+  name         = "cloudnativedays"
+  organization = "kusama"
+}
+
 variable "oauth_token_id" {
   default = "ot-TXWpAfuW6HpR3aCT"
 }
@@ -21,6 +26,12 @@ resource "tfe_workspace" "nextcloud" {
   }
 }
 
+resource "tfe_team_access" "nextcloud" {
+  access       = "admin"
+  team_id      = data.tfe_team.cloudnativedays.id
+  workspace_id = tfe_workspace.nextcloud.id
+}
+
 resource "tfe_workspace" "github" {
   name                = "github"
   organization        = data.tfe_organization.org.name
@@ -35,3 +46,10 @@ resource "tfe_workspace" "github" {
     oauth_token_id     = var.oauth_token_id
   }
 }
+
+resource "tfe_team_access" "github" {
+  access       = "admin"
+  team_id      = data.tfe_team.cloudnativedays.id
+  workspace_id = tfe_workspace.github.id
+}
+
