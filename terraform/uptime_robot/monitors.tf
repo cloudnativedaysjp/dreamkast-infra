d794c34490a2bf4020ab1ba503633bdd14f204be
Kazuto Kusama <jacopen@gmail.com>
2022-02-17 10:20:31 +0900

    Update monitors.tf

--- terraform/uptime_robot/monitors.tf
+++ terraform/uptime_robot/monitors.tf
@@ -1,6 +1,6 @@
 # Create a monitor
 resource "uptimerobot_monitor" "dreamkast-by-terraform" {
-  friendly_name = "Dreamkast by Terraform"
+  friendly_name = "Dreamkast"
   type          = "http"
   url           = "https://event.cloudnativedays.jp"
 
@@ -22,9 +22,31 @@ resource "uptimerobot_monitor" "dreamkast-by-terraform" {
 }
 
 resource "uptimerobot_monitor" "website-by-terraform" {
-  friendly_name = "Website by Terraform"
+  friendly_name = "Website"
   type          = "http"
   url           = "https://cloudnativedays.jp"
+
+  alert_contact {
+    id = uptimerobot_alert_contact.slack.id
+  }
+
+  alert_contact {
+    id = data.uptimerobot_alert_contact.default_alert_contact.id
+  }
+
+  interval = 60
+
+  lifecycle {
+    ignore_changes = [
+      alert_contact
+    ]
+  }
+}
+
+resource "uptimerobot_monitor" "grafana" {
+  friendly_name = "Grafana"
+  type          = "http"
+  url           = "https://grafana.cloudnativedays.jp"
 
   alert_contact {
     id = uptimerobot_alert_contact.slack.id
