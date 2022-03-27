ab61b695aa917969cfaf630ae21ed50407626bd5
Kazuto Kusama <jacopen@gmail.com>
2022-03-27 18:24:30 +0900

    Fix FW rule

--- terraform/broadcast-switcher/networking.tf
+++ terraform/broadcast-switcher/networking.tf
@@ -47,6 +47,21 @@ resource "sakuracloud_packet_filter_rules" "switcher_rules" {
   }
 
   expression {
+    protocol    = "udp"
+    source_port = "123"
+  }
+
+  expression {
+    protocol         = "tcp"
+    destination_port = "32768-61000"
+  }
+
+  expression {
+    protocol         = "udp"
+    destination_port = "32768-61000"
+  }
+
+  expression {
     protocol    = "ip"
     allow       = false
     description = "Deny ALL"
--- terraform/nextcloud/networking.tf
+++ terraform/nextcloud/networking.tf
@@ -30,6 +30,20 @@ resource "sakuracloud_packet_filter_rules" "nextcloud_rules" {
   }
 
   expression {
+    protocol    = "udp"
+    source_port = "123"
+  }
+
+  expression {
+    protocol         = "tcp"
+    destination_port = "32768-61000"
+  }
+
+  expression {
+    protocol         = "udp"
+    destination_port = "32768-61000"
+  }
+  expression {
     protocol    = "ip"
     allow       = false
     description = "Deny ALL"
