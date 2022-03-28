8d650767496c9dc9f7485d368a2ae967ca847568
Kazuto Kusama <jacopen@gmail.com>
2022-03-28 23:00:35 +0900

    Update nextcloud

--- terraform/nextcloud/instance.tf
+++ terraform/nextcloud/instance.tf
@@ -78,7 +78,7 @@ resource "sakuracloud_disk" "nextcloud_boot2" {
   source_archive_id = data.sakuracloud_archive.ubuntu.id
   plan              = "ssd"
   connector         = "virtio"
-  size              = 40
+  size              = 1024
 
   lifecycle {
     ignore_changes = [
