908edd078194eb71ff4817364e44a80752ecd1ef
Kazuto Kusama <jacopen@gmail.com>
2022-03-31 22:46:19 +0900

    Add route53 record

--- terraform/broadcast-switcher/route53.tf
+++ terraform/broadcast-switcher/route53.tf
@@ -11,3 +11,12 @@ resource "aws_route53_record" "switcher" {
   ttl      = "300"
   records  = [sakuracloud_server.switcher[each.key].ip_address]
 }
+
+resource "aws_route53_record" "instances" {
+  for_each = { for i in local.instances : i.hostname => i }
+  zone_id  = data.aws_route53_zone.cloudnativedays.zone_id
+  name     = "${each.key}.cloudnativedays.jp"
+  type     = "A"
+  ttl      = "300"
+  records  = [sakuracloud_server.instances[each.key].ip_address]
+}
