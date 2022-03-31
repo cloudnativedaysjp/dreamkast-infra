61af64fefd6395fb9efb8a4ee1965d8a4cd1c3b9
Kazuto Kusama <jacopen@gmail.com>
2022-03-31 21:12:56 +0900

    Fix duplicated data

--- terraform/broadcast-switcher/outputs.tf
+++ terraform/broadcast-switcher/outputs.tf
@@ -1,7 +1,3 @@
-data "sakuracloud_server_vnc_info" "instances" {
-  server_id = sakuracloud_server.foobar.id
-}
-
 data "sakuracloud_server_vnc_info" "switcher" {
   for_each = { for i in local.switchers : i.hostname => i }
   server_id = sakuracloud_server.switcher[each.key].id
