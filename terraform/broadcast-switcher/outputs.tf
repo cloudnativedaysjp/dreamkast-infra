c8f08d43c4f015a5aad3244cab62d33add84c4d4
Kazuto Kusama <jacopen@gmail.com>
2022-03-31 21:16:46 +0900

    remove vnc data resource

--- terraform/broadcast-switcher/outputs.tf
+++
@@ -1,9 +0,0 @@
-data "sakuracloud_server_vnc_info" "switcher" {
-  for_each = { for i in local.switchers : i.hostname => i }
-  server_id = sakuracloud_server.switcher[each.key].id
-}
-
-data "sakuracloud_server_vnc_info" "instances" {
-  for_each = { for i in local.instances : i.hostname => i }
-  server_id = sakuracloud_server.instances[each.key].id
-}
