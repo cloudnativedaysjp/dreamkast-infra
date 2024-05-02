91c2e4afa09511e61a3479ef81a09ee939ba689f
GitHub Action <action@github.com>
2024-05-02 15:26:19 +0000

    Bump docker tag (https://github.com/cloudnativedaysjp/dreamkast/commit/976067ff843a4f7db9739dab0dddae3aa5ae93a9)

---
+++ ecspresso/reviewapps/dk-2267/cleanup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)
+find . -name "ecspresso.yml" | xargs -I{} -P10 ecspresso --config={} delete --force --terminate
+aws servicediscovery delete-service --id srv-oqkipfbr4ziqkm3m
+aws servicediscovery delete-service --id srv-jmaafqsyirfxzub3
+aws elbv2 delete-rule --rule-arn arn:aws:elasticloadbalancing:us-west-2:607167088920:listener-rule/app/dreamkast-dev/122c5b4a47b64f9d/bc86e7b2e4bca8f5/19a3713578eac42a
+aws elbv2 delete-target-group --target-group-arn arn:aws:elasticloadbalancing:us-west-2:607167088920:targetgroup/dreamkast-dev-dk-2267-dreamkast/6cf30fd360d2434b
