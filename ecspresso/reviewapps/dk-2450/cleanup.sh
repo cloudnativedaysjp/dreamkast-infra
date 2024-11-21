41f3048ca253e433e5a395b570774c927bd5b198
GitHub Action <action@github.com>
2024-11-21 12:43:18 +0000

    Bump docker tag by https://github.com/cloudnativedaysjp/dreamkast/pull/2450 commit: https://github.com/cloudnativedaysjp/dreamkast/commit/2b5ab0638954962a18a8e4d7781b265523f1bb57

---
+++ ecspresso/reviewapps/dk-2450/cleanup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e -o pipefail
+cd $(dirname $0)
+
+find . -name "ecspresso.yml" | xargs -I{} -P10 ecspresso --config={} delete --force --terminate ||:
+sleep 10 # wait for ECS Services to be deleted
+aws events delete-rule --name dk-2450-harvestjob
+aws ecs deregister-task-definition --task-definition dreamkast-dev-dk-2450-harvestjob
+aws servicediscovery get-service --id srv-mrugnf3fzlt36l55 &>/dev/null && aws servicediscovery delete-service --id srv-mrugnf3fzlt36l55
+aws servicediscovery get-service --id srv-vlrydf7zrxvls2fj &>/dev/null && aws servicediscovery delete-service --id srv-vlrydf7zrxvls2fj
+aws elbv2 describe-rules --rule-arn arn:aws:elasticloadbalancing:us-west-2:607167088920:listener-rule/app/dreamkast-dev/122c5b4a47b64f9d/bc86e7b2e4bca8f5/3597a0ee4d865a45 &>/dev/null && aws elbv2 delete-rule --rule-arn arn:aws:elasticloadbalancing:us-west-2:607167088920:listener-rule/app/dreamkast-dev/122c5b4a47b64f9d/bc86e7b2e4bca8f5/3597a0ee4d865a45
+aws elbv2 describe-target-groups --target-group-arn arn:aws:elasticloadbalancing:us-west-2:607167088920:targetgroup/dev-dk-2450/f3aee4c409bd23cd &>/dev/null && aws elbv2 delete-target-group --target-group-arn arn:aws:elasticloadbalancing:us-west-2:607167088920:targetgroup/dev-dk-2450/f3aee4c409bd23cd
+:
