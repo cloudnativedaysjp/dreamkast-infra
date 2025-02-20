aa922abf88b775c33dc4511ec895ceac749c497f
GitHub Action <action@github.com>
2025-02-20 14:21:45 +0000

    Bump docker tag by https://github.com/cloudnativedaysjp/dreamkast-weaver/pull/106 commit: https://github.com/cloudnativedaysjp/dreamkast-weaver/commit/cd5d7387fb2213d060e8cca5fb4d3491d1eb9dd1

---
+++ ecspresso/reviewapps/weaver-106/cleanup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e -o pipefail
+cd $(dirname $0)
+
+find . -name "ecspresso.jsonnet" | xargs -I{} -P10 ecspresso --config={} delete --force --terminate ||:
+sleep 10 # wait for ECS Services to be deleted
+aws servicediscovery get-service --id srv-n3o7df5linw7hhoz &>/dev/null && aws servicediscovery delete-service --id srv-n3o7df5linw7hhoz
+aws elbv2 describe-rules --rule-arn arn:aws:elasticloadbalancing:us-west-2:607167088920:listener-rule/app/dreamkast-dev/122c5b4a47b64f9d/bc86e7b2e4bca8f5/c72666fe0bb96bbe &>/dev/null && aws elbv2 delete-rule --rule-arn arn:aws:elasticloadbalancing:us-west-2:607167088920:listener-rule/app/dreamkast-dev/122c5b4a47b64f9d/bc86e7b2e4bca8f5/c72666fe0bb96bbe
+aws elbv2 describe-target-groups --target-group-arn arn:aws:elasticloadbalancing:us-west-2:607167088920:targetgroup/dev-weaver-106/42279a3e932b23f3 &>/dev/null && aws elbv2 delete-target-group --target-group-arn arn:aws:elasticloadbalancing:us-west-2:607167088920:targetgroup/dev-weaver-106/42279a3e932b23f3
+:
